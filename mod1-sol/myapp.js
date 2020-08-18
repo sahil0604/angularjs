970e48bde0772ca6c610e2cd19623411a5a02189
Sahil Shaikh <sahilshaikhsss786@gmail.com>
2020-08-18 17:30:56 +0530

    mod1-update

--- mod1-sol/myapp.js
+++ mod1-sol/myapp.js
@@ -6,7 +6,10 @@ angular.module('MyApp', [])
     $scope.output="";
     $scope.check= function(){
     var N = count($scope.launch) + 1;
-    if (N <= 3) {
+    if(N==1) {
+        $scope.output = "Please enter data first"
+    }
+    else if (N > 1 && N <= 3) {
         $scope.output= "Enjoy!";
     }
     else {
@@ -14,15 +17,19 @@ angular.module('MyApp', [])
     }
     };
     var count = function(string){
-        var c=0
+        var c=0;
+        if(string == undefined || string =="") {
+            return c;
+        }
+        else {
         for (var i=0; i < string.length; i++) {
             if(string[i]==",") {
                 c+=1;
             }
         }
         return c;
+     }
     };
 });
-    
 })();
     
